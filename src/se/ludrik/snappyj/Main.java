1253912b6d0469155ccff0a2713a4ca990c11995
Erik Haqvinsson <erik.h@sendus.se>
2014-03-25 13:19:29 +0100

    a

--- src/se/ludrik/snappyj/Main.java
+++ src/se/ludrik/snappyj/Main.java
@@ -40,7 +40,7 @@ public class Main {
   }
 
   public void init() throws IOException {
-    fIn = new FileInputStream("test2.java");
+    fIn = new FileInputStream("test.java");
     input = new ANTLRInputStream(fIn);
     lexer = new SnappyJavaLexer(input);
     tokens = new CommonTokenStream(lexer);
