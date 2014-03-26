b228cf198fdf35b20a19f4894d7d540613b71b2c
Erik Haqvinsson <erikhaq@gmail.com>
2014-03-26 13:52:12 +0100

    add bash test file

--- src/mjc/JVMMain.java
+++ src/mjc/JVMMain.java
@@ -4,16 +4,30 @@ import se.ludrik.snappyj.*;
 
 /**
  * Run from SnappyJava directory with:
- * java -cp build/jar/mjc.jar:lib/antlr-4.2-complete.jar mjc.JVMMain test-files/file-name.java
+ * java -cp build/jar/mjc.jar:lib/antlr-4.2-complete.jar mjc.JVMMain [filename] [flags]
  */
 public class JVMMain {
+
+  public static boolean generateAssemblyCode = false;
+  public static boolean printErrors = true;
+
   public static void main(String[] args) {
     String fileName;
-    if(args.length != 1) {
-      System.err.println("No file specified, using default test.java");
-      fileName = "test.java";
-    } else {
+    if(args.length > 0) {
       fileName = args[0];
+      if(args.length == 2) {
+        if(args[1].equals("-n")) {
+          printErrors = false;
+        } else if(args[1].equals("-S")) {
+          generateAssemblyCode = true;
+        }
+      }
+    } else {
+      //System.err.println("Please specify a filename");
+      System.err.println("Using default file.");
+      fileName = "test.java";
+      //System.exit(1);
+      //return;
     }
 
     try {
@@ -22,6 +36,7 @@ public class JVMMain {
     } catch (IOException e) {
       e.printStackTrace();
     }
+
   }
 
 }
--- src/se/ludrik/snappyj/ErrorHandler.java
+++ src/se/ludrik/snappyj/ErrorHandler.java
@@ -1,31 +1,46 @@
 package se.ludrik.snappyj;
 
+import mjc.JVMMain;
 import org.antlr.v4.runtime.Token;
+import mjc.JVMMain.*;
 
 
 /**
  * Created by erikhaq on 2014-03-21.
  */
 public class ErrorHandler {
+  private static boolean errorDetected;
   public ErrorHandler() {
+    errorDetected = false;
+  }
+
+  private static void setErrorDetected() {
+    if(!errorDetected) errorDetected = true;
+  }
+
+  public static boolean getErrorDetected() {
+    return errorDetected;
   }
 
   public static void variableAlreadyDefinedInMethod(Token idToken, String methodName) {
     //Error:(40, 17) java: variable symTable is already defined in method init()
-    System.err.printf("Error:(%d, %d) java: variable '%s' is already defined in method %s()\n",
+    if(JVMMain.printErrors) System.err.printf("Error:(%d, %d) java: variable '%s' is already defined in method %s()\n",
         idToken.getLine(), idToken.getCharPositionInLine(), idToken.getText(), methodName);
+    setErrorDetected();
   }
 
   public static void variableAlreadyDefinedInClass(Token idToken, String className) {
     //Error:(19, 27) java: variable lexer is already defined in class se.ludrik.snappyj.Main
-    System.err.printf("Error:(%d, %d) java: variable '%s' is already defined in class %s\n",
+    if(JVMMain.printErrors) System.err.printf("Error:(%d, %d) java: variable '%s' is already defined in class %s\n",
         idToken.getLine(), idToken.getCharPositionInLine(), idToken.getText(), className);
+    setErrorDetected();
   }
 
   public static void methodAlreadyDefined(Token idToken, String className) {
     //Error:(63, 15) java: method init() is already defined in class se.ludrik.snappyj.Main
-    System.err.printf("Error:(%d, %d) java: method %s() is already defined in class %s\n",
+    if(JVMMain.printErrors) System.err.printf("Error:(%d, %d) java: method %s() is already defined in class %s\n",
         idToken.getLine(), idToken.getCharPositionInLine(), idToken.getText(), className);
+    setErrorDetected();
   }
   public static void noSuchMethod(Token methodIdToken, String className) {
     System.err.printf("Error:(%d, %d) java: no such method %s() defined in class %s\n",
@@ -47,49 +62,46 @@ public class ErrorHandler {
   }
   public static void classAlreadyDefined(Token idToken) {
     //Error:(51, 8) java: duplicate class: se.ludrik.snappyj.Main
-    System.err.printf("Error:(%d, %d) java: duplicate class: %s\n", idToken.getLine(),
+    if(JVMMain.printErrors) System.err.printf("Error:(%d, %d) java: duplicate class: %s\n", idToken.getLine(),
         idToken.getCharPositionInLine(), idToken.getText());
+    setErrorDetected();
   }
 
   public static void incompatibleTypes(Token idToken, String requiredType, String foundType) {
     //Error:(42, 12) java: incompatible types
     //required: boolean
     //found:    int
-    System.err.printf(
+    if(JVMMain.printErrors) System.err.printf(
         "Error:(%d, %d) java: incompatible types\n\trequired:\t%s\n\tfound:\t\t%s\n",
         idToken.getLine(), idToken.getCharPositionInLine(), requiredType, foundType);
+    setErrorDetected();
   }
 
   public static void missingClassSymbol(Token typeToken, String location) {
     //Error:(43, 5) java: cannot find symbol
     //symbol:   class Test
     //location: class se.ludrik.snappyj.Main
-    System.err.printf(
+    if(JVMMain.printErrors) System.err.printf(
         "Error:(%d, %d) java: cannot find symbol\n\tsymbol:\t\tclass %s\n\tlocation:\tclass %s\n",
         typeToken.getLine(), typeToken.getCharPositionInLine(), typeToken.getText(), location);
+    setErrorDetected();
   }
 
   public static void missingVariableSymbol(Token idToken, String location) {
     //Error:(42, 5) java: cannot find symbol
     //symbol:   variable testVar
     //location: class se.ludrik.snappyj.Main
-    System.err.printf(
+    if(JVMMain.printErrors) System.err.printf(
         "Error:(%d, %d) java: cannot find symbol\n\tsymbol:\t\tvariable %s\n\tlocation:\t%s\n",
         idToken.getLine(), idToken.getCharPositionInLine(), idToken.getText(), location);
+    setErrorDetected();
   }
 
   public static void notAStatement(Token exprToken) {
     //Error:(52, 13) java: not a statement
-    System.err.printf("Error:(%d, %d) java: not a statement\n", exprToken.getLine(),
+    if(JVMMain.printErrors) System.err.printf("Error:(%d, %d) java: not a statement\n", exprToken.getLine(),
         exprToken.getCharPositionInLine());
+    setErrorDetected();
   }
-
-  private class Error {
-
-    public Error() {
-
-    }
-  }
-
 
 }
